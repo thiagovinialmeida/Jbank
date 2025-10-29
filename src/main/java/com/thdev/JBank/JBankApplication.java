3bad801f6af2c1da518847e6f924b461a9dc1063
ThiagoVini-S <th.vinialmeida@gmail.com>
2025-10-29 19:59:22 -0300

    Feat: First Commit

---
+++ src/main/java/com/thdev/JBank/JBankApplication.java
@@ -0,0 +1,13 @@
+package com.thdev.JBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBankApplication.class, args);
+	}
+
+}
